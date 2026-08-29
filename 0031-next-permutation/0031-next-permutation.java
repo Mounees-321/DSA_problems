bbc0cf758af6b638cd47f3594ff509a5aac9ddd0
Mouneeswaran S <161322920+Mounees-321@users.noreply.github.com>
2026-08-29 23:41:23 +0530

    Time: 0 ms (100%), Space: 44.4 MB (93.22%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,33 +1,30 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int n = nums.length;
-        int i=n-2;
-        while ((i>=0) && nums[i+1]<=nums[i])
-        {
+        int i = n - 2;
+        while ((i >= 0) && nums[i + 1] <= nums[i]) {
             i--;
         }
-        if(i>=0)
-        {
-            int j = n-1;
-            while(nums[i]>=nums[j])
-            {
+        if (i >= 0) {
+            int j = n - 1;
+            while (nums[i] >= nums[j]) {
                 j--;
             }
             int temp = nums[j];
-            nums[j] =nums[i];
+            nums[j] = nums[i];
             nums[i] = temp;
         }
 
-i = i+1;
-int j = n-1;
-while (i < j) {
-    int temp = nums[i];
-    nums[i] = nums[j];
-    nums[j] = temp;
+        i = i + 1;
+        int j = n - 1;
+        while (i < j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
 
-    i++;
-    j--;
-}
-        
+            i++;
+            j--;
+        }
+
     }
 }
